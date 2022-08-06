6538b08995a0bdc59162a38a5c6c7bb1b84433b2
Justin Horein <justinhorein@gmail.com>
2022-08-05 18:43:24 -0700

    ui looks good

--- App.java
+++ App.java
@@ -9,34 +9,30 @@ public class App {
         JFrame frame = new JFrame("Calculator");
         frame.setLayout(null);
 
+        JTextField out = new JTextField("0");
+        out.setBounds(10, 10, 230, 50);
+        out.setEditable(false);
+        frame.add(out);
+
         // Buttons
         // First Row
-        JButton b1 = new JButton("1");
-        b1.setBounds(10, 10, 50, 50);
-        frame.add(b1);
+        JButton bplus = new JButton("+");
+        bplus.setBounds(10, 70, 50, 50);
+        frame.add(bplus);
 
-        JButton b2 = new JButton("2");
-        b2.setBounds(70, 10, 50, 50);
-        frame.add(b2);
+        JButton bminus = new JButton("-");
+        bminus.setBounds(70, 70, 50, 50);
+        frame.add(bminus);
 
-        JButton b3 = new JButton("3");
-        b3.setBounds(130, 10, 50, 50);
-        frame.add(b3);
+        JButton bmult = new JButton("x");
+        bmult.setBounds(130, 70, 50, 50);
+        frame.add(bmult);
+
+        JButton bdiv = new JButton("÷");
+        bdiv.setBounds(190, 70, 50, 50);
+        frame.add(bdiv);
 
         // Second Row
-        JButton b4 = new JButton("4");
-        b4.setBounds(10, 70, 50, 50);
-        frame.add(b4);
-
-        JButton b5 = new JButton("5");
-        b5.setBounds(70, 70, 50, 50);
-        frame.add(b5);
-
-        JButton b6 = new JButton("6");
-        b6.setBounds(130, 70, 50, 50);
-        frame.add(b6);
-
-        // Third Row
         JButton b7 = new JButton("7");
         b7.setBounds(10, 130, 50, 50);
         frame.add(b7);
@@ -49,8 +45,52 @@ public class App {
         b9.setBounds(130, 130, 50, 50);
         frame.add(b9);
 
-        frame.setPreferredSize(new Dimension(190, 300));
-        frame.setBackground(Color.BLUE);
+        JButton bequals = new JButton("=");
+        bequals.setBounds(190, 130, 50, 230);
+        frame.add(bequals);
+
+        // Third Row
+        JButton b4 = new JButton("4");
+        b4.setBounds(10, 190, 50, 50);
+        frame.add(b4);
+
+        JButton b5 = new JButton("5");
+        b5.setBounds(70, 190, 50, 50);
+        frame.add(b5);
+
+        JButton b6 = new JButton("6");
+        b6.setBounds(130, 190, 50, 50);
+        frame.add(b6);
+
+        // Fourth Row
+        JButton b1 = new JButton("1");
+        b1.setBounds(10, 250, 50, 50);
+        frame.add(b1);
+
+        JButton b2 = new JButton("2");
+        b2.setBounds(70, 250, 50, 50);
+        frame.add(b2);
+
+        JButton b3 = new JButton("3");
+        b3.setBounds(130, 250, 50, 50);
+        frame.add(b3);
+
+        // Fifth Row
+
+        JButton b0 = new JButton("0");
+        b0.setBounds(10, 310, 50, 50);
+        frame.add(b0);
+
+        JButton bdot = new JButton(".");
+        bdot.setBounds(70, 310, 50, 50);
+        frame.add(bdot);
+
+        JButton bclear = new JButton("AC");
+        bclear.setBounds(130, 310, 50, 50);
+        frame.add(bclear);
+
+
+        frame.setPreferredSize(new Dimension(250, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.pack();
