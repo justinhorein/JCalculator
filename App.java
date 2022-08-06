1d400f009c8dd689c663078dcf88aa6e001e53ba
Justin Horein <justinhorein@gmail.com>
2022-08-06 16:56:58 -0700

    functioning properly

--- App.java
+++ App.java
@@ -134,6 +134,59 @@ public class App extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
 
-        out.setText(s);
+        if (s.charAt(0) >= '0' && s.charAt(0) <= '9' || s.charAt(0) == '.') {
+            if (!s1.equals(""))
+                s2 = s2 + s;
+            else
+                s0 = s0 + s;
+
+            out.setText(s0 + s1 + s2);
+        }
+        else if (s.charAt(0) == 'A') {
+            s0 = s1 = s2 = "";
+
+            out.setText(s0 + s1 + s2);
+        }
+        else if (s.charAt(0) == '=') {
+            double te;
+
+            if (s1.equals("+"))
+                te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+            else if (s1.equals("-"))
+                te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+            else if (s1.equals("÷"))
+                te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+            else
+                te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+
+            out.setText(s0 + s1 + s2 + "=" + te);
+
+            s0 = Double.toString(te);
+
+            s1 = s2 = "";
+        }
+        else {
+            if (s1.equals("") || s2.equals(""))
+                s1 = s;
+            else {
+                double te;
+
+                if (s1.equals("+"))
+                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
+                else if (s1.equals("-"))
+                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
+                else if (s1.equals("÷"))
+                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                else
+                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+
+                s0 = Double.toString(te);
+                s1 = s;
+                s2 = "";
+
+            }
+
+            out.setText(s0 + s1 + s2);
+        }
     }
 }
